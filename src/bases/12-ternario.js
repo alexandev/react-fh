53ecc8a777e4337c5a6b86badf1fae4087d3a16f
Alexander Moreno <alexander.morenoc@outlook.com>
2021-09-24 14:24:39 -0500

    operador ternario

---
+++ src/bases/12-ternario.js
@@ -0,0 +1,13 @@
+const activo = true;
+// let mensaje = '';
+
+// if(activo){
+//     mensaje = 'activo';
+// }else{
+//     mensaje = 'inactivo';
+// }
+
+// const mensaje = activo ? 'activo':'inactivo';
+const mensaje = activo && 'activo';
+
+console.log(mensaje);
