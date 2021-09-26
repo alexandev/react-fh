fb606f6554901029895cf2b1f7832b985ae8d5e4
Alexander <alexander.morenoc@outlook.com>
2021-09-26 17:57:55 -0500

    props

---
+++ src/PrimeraApp.js
@@ -0,0 +1,19 @@
+
+import React from 'react';
+
+//fc
+const PrimeraApp = ({ saludo }) =>{
+
+    // const saludo = 'Hola Mundo';
+
+    
+
+    return(
+        <>
+            <h1>{ saludo }</h1>
+            <p>Mi primera aplicación</p>
+        </>
+    );
+}
+
+export default PrimeraApp;
--- src/bases/01-const-let.js
+++
@@ -1,9 +0,0 @@
-console.log('hola mundo')
-
-//variables y constantes 
-const nombre= 'Fernando';
-const apellido = 'Herrera';
-
-let valorDado = 5;
-
-console.log(nombre, apellido, valorDado)
